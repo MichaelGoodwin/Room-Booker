19f784f3bdcaa02f487135dfff1ddc105804746e
MichaelGoodwin <7521194+MichaelGoodwin@users.noreply.github.com>
2019-10-23 21:53:00 -0700

    Add Bootstrap and FontAwesome dependencies

--- Room-Booker/src/app/app.module.ts
+++ Room-Booker/src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Third-party resources
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+// App resources
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,6 +14,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FontAwesomeModule,
     AppRoutingModule
   ],
   providers: [],
